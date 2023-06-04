71e04e11ef273f3d2ebc2c90c9c7ba31d7543695
ramsai24 <jnrsai24@gmail.com>
2023-06-04 08:31:57 +0000

    css and javascript file working properly in all type devices and test cases passed

--- src/components/deleteItems/index.js
+++ src/components/deleteItems/index.js
@@ -14,11 +14,20 @@ const HistoryItem = props => {
       <p>{item.timeAccessed}</p>
       <div className="url-title-delete-container">
         <div className="history-item">
-          <img src={item.logoUrl} alt="domain logo" />
-          <p>{item.title}</p>
-          <p>{item.domainUrl}</p>
+          <div className="url-container">
+            <img className="img-logo" src={item.logoUrl} alt="domain logo" />
+            <div className="title-url-container">
+              <p className="title">{item.title}</p>
+              <p className="url">{item.domainUrl}</p>
+            </div>
+          </div>
         </div>
-        <button data-testid="delete" type="button" onClick={onDeleteItem}>
+        <button
+          className="deletebtn"
+          data-testid="delete"
+          type="button"
+          onClick={onDeleteItem}
+        >
           <img
             alt="delete"
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
